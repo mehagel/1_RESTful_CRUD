8df5c49b880c2e919a021a57ecf75ef7a42e00fe
Apprentice <apprentice@devbootcamp.com>
2013-07-20 16:46:17 -0700

    fixed database

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,27 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+post '/create_note' do
+  p params
+  # note = Note.create(tile: pararms[:title], content: parmams[:content])
+
+
+  erb :index
+end
+
+
+post '/edit_note' do
+
+
+
+
+   erb :index
+ end
+
+post '/delete_note' do
+
+  # Note.find(params[:id]).destroy
+
+   erb :index
+ end

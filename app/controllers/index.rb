6ff4829bd62e85f96b0aec5604c83e8f2b0f3c2d
Apprentice <apprentice@devbootcamp.com>
2013-07-21 21:01:56 -0700

    changes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,20 +1,22 @@
 get '/' do
-  # Look in app/views/index.erb
-  @notes = Note.all
 
   erb :index
 end
 
+
 post '/create_note' do
+  @notes = Note.all
   Note.create(params)
   erb :note
 end
 
-# get '/note/id'
-# # p params[:id]
-# # # @note=Note.find(params[:id])
-# # erb :note
-#  redirect '/'
+get '/note/id'
+@notes = Note.all
+p params[:id]
+# @note=Note.find(params[:id])
+erb :note
+ redirect '/'
+end
 
 
 # post '/edit_note' do
